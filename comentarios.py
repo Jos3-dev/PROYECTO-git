94d9844346598385ac1242a1fcc119cc4e9389c5
Jose Angel Sanchez Ramos <97856392+Jos3-dev@users.noreply.github.com>
2022-12-11 13:33:37 -0800

    Add files via upload

---
+++ comentarios.py
@@ -0,0 +1,2 @@
+print("esto es solo una prueba para los comentarios")
+# esto es todo un comentario que python no lo ejecuta, lo omite
---
+++ holamundo.py
@@ -0,0 +1,18 @@
+# print("hola mundo, esto es una primera prueba!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# definimos el problema
+pensando = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nLos Sabios se ocultan en su impenetrabilidad, a fin de que sus sentimientos no puedan ser observados, operan bajo la ausencia de una forma definida, a fin de que sus lineas no puedan ser cruzadas.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n....."
+print(pensando)
